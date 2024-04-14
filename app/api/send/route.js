62b048af23311a7443cb7d4406273c94404ac478
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 14:17:50 -0400

    vercel email still broken

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,55 +1,55 @@
-// import { NextResponse } from "next/server";
-// import { Resend } from "resend";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
 
-// export async function POST(req, res) {
-//   const { email, subject, message } = await req.json();
-//   console.log(email, subject, message);
-//   try {
-//     const data = await resend.emails.send({
-//       from: fromEmail,
-//       to: [toEmail],
-//       subject: subject,
-//       react: (
-//         <>
-//           <p>From: {email}</p>
-//           <h1>{subject}</h1>
-//           <p>Thank you for contacting me!</p>
-//           <p>New message submitted:</p>
-//           <p>{message}</p>
-//         </>
-//       ),
-//     });
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return NextResponse.json({ error });
-//   }
-// }
-import { NextResponse } from "next/server";
-
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
-export async function POST() {
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
       from: fromEmail,
       to: [toEmail],
-      subject: "hello world",
-      html: "<strong>it works!</strong>",
-    }),
-  });
-
-  if (res.ok) {
-    const data = await res.json();
+      subject: subject,
+      react: (
+        <>
+          <p>From: {email}</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting me!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
     return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
   }
 }
+// import { NextResponse } from "next/server";
+
+// export const runtime = "edge";
+// export const dynamic = "force-dynamic";
+
+// export async function POST() {
+//   const res = await fetch("https://api.resend.com/emails", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+//     },
+//     body: JSON.stringify({
+//       from: fromEmail,
+//       to: [toEmail],
+//       subject: "hello world",
+//       html: "<strong>it works!</strong>",
+//     }),
+//   });
+
+//   if (res.ok) {
+//     const data = await res.json();
+//     return NextResponse.json(data);
+//   }
+// }
