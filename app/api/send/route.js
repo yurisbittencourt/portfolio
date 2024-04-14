052dd1b183ec7432ddf688195e0ba81468d5c368
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 14:25:50 -0400

    vercel email???

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -28,28 +28,3 @@ export async function POST(req, res) {
     return NextResponse.json({ error });
   }
 }
-// import { NextResponse } from "next/server";
-
-// export const runtime = "edge";
-// export const dynamic = "force-dynamic";
-
-// export async function POST() {
-//   const res = await fetch("https://api.resend.com/emails", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
-//     },
-//     body: JSON.stringify({
-//       from: fromEmail,
-//       to: [toEmail],
-//       subject: "hello world",
-//       html: "<strong>it works!</strong>",
-//     }),
-//   });
-
-//   if (res.ok) {
-//     const data = await res.json();
-//     return NextResponse.json(data);
-//   }
-// }
