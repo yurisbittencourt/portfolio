7f93a0f52ad4e9eefea4d9340ca72c3c36698c40
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 13:24:01 -0400

    email fix

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -3,6 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
+const toEmail = process.env.TO_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
@@ -10,12 +11,13 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email],
+      to: [toEmail, email],
       subject: subject,
       react: (
         <>
+          <p>From: {email}</p>
           <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
+          <p>Thank you for contacting me!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
