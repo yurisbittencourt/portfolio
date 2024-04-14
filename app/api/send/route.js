5d18e87728b68c64847a14ed4dee7125bb4d666b
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 13:56:41 -0400

    email vercel test

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-const toEmail = process.env.TO_EMAIL;
+// const fromEmail = process.env.FROM_EMAIL;
+// const toEmail = process.env.TO_EMAIL;
 
 // export async function POST(req, res) {
 //   const { email, subject, message } = await req.json();
@@ -11,11 +11,10 @@ const toEmail = process.env.TO_EMAIL;
 //   try {
 //     const data = await resend.emails.send({
 //       from: fromEmail,
-//       to: [toEmail, email],
+//       to: [toEmail],
 //       subject: subject,
 //       react: (
 //         <>
-//           <p>From: {email}</p>
 //           <h1>{subject}</h1>
 //           <p>Thank you for contacting me!</p>
 //           <p>New message submitted:</p>
@@ -29,13 +28,14 @@ const toEmail = process.env.TO_EMAIL;
 //   }
 // }
 
+import { NextResponse } from "next/server";
+
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 const RESEND_API_KEY = "re_123456789";
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
+export async function POST() {
   const res = await fetch("https://api.resend.com/emails", {
     method: "POST",
     headers: {
@@ -43,18 +43,10 @@ export async function POST(req, res) {
       Authorization: `Bearer ${RESEND_API_KEY}`,
     },
     body: JSON.stringify({
-      from: fromEmail,
-      to: [toEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <p>From: {email}</p>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting me!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
+      from: "Acme <onboarding@resend.dev>",
+      to: ["delivered@resend.dev"],
+      subject: "hello world",
+      html: "<strong>it works!</strong>",
     }),
   });
 
