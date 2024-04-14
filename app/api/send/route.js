33bc8bda28c37af658ed1b0a8a28bfcce713afec
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 13:43:27 -0400

    vercel email fix?

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,15 +1,48 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
 
+// export async function POST(req, res) {
+//   const { email, subject, message } = await req.json();
+//   console.log(email, subject, message);
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: [toEmail, email],
+//       subject: subject,
+//       react: (
+//         <>
+//           <p>From: {email}</p>
+//           <h1>{subject}</h1>
+//           <p>Thank you for contacting me!</p>
+//           <p>New message submitted:</p>
+//           <p>{message}</p>
+//         </>
+//       ),
+//     });
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = "re_123456789";
+
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-  try {
-    const data = await resend.emails.send({
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
       from: fromEmail,
       to: [toEmail, email],
       subject: subject,
@@ -22,9 +55,11 @@ export async function POST(req, res) {
           <p>{message}</p>
         </>
       ),
-    });
+    }),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
     return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
   }
 }
