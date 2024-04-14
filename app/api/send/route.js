97e49e0e7fe64636adb34e5c57ea48d1909beb5b
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-14 14:00:16 -0400

    vercel email fix??

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,57 +1,32 @@
-// import { NextResponse } from "next/server";
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-// const fromEmail = process.env.FROM_EMAIL;
-// const toEmail = process.env.TO_EMAIL;
-
-// export async function POST(req, res) {
-//   const { email, subject, message } = await req.json();
-//   console.log(email, subject, message);
-//   try {
-//     const data = await resend.emails.send({
-//       from: fromEmail,
-//       to: [toEmail],
-//       subject: subject,
-//       react: (
-//         <>
-//           <h1>{subject}</h1>
-//           <p>Thank you for contacting me!</p>
-//           <p>New message submitted:</p>
-//           <p>{message}</p>
-//         </>
-//       ),
-//     });
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return NextResponse.json({ error });
-//   }
-// }
-
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+const toEmail = process.env.TO_EMAIL;
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
-const RESEND_API_KEY = "re_123456789";
-
-export async function POST() {
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["delivered@resend.dev"],
-      subject: "hello world",
-      html: "<strong>it works!</strong>",
-    }),
-  });
-
-  if (res.ok) {
-    const data = await res.json();
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [toEmail],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting me!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
     return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
   }
 }
