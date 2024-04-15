1e6a1885d7929b12dff45db06075c27e7ec8d61e
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-15 01:03:15 -0400

    resume link fix

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -55,14 +55,15 @@ const HeroSection = () => {
             >
               Message me!
             </Link>
-            <Link
+            <a
               href="/cv.pdf"
+              target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 View Resume
               </span>
-            </Link>
+            </a>
           </div>
         </motion.div>
         <motion.div
