d87b4f88378263e9099eed06d5adf2ff3bc30f68
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-15 01:27:26 -0400

    added links

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -55,10 +55,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/yurisbittencourt">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/ysbittencourt/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
